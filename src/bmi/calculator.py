41344a508a26dee042a09be835de050af03ce3f6
Eulogio Gutierrez <eulogio123@googlemail.com>
2020-09-14 15:12:34 +0100

    json errors

--- src/bmi/calculator.py
+++ src/bmi/calculator.py
@@ -13,10 +13,10 @@ GENERIC_ERROR = "Internal error"
 
 @cors()
 @log(parameters=True, response=True)
-@response_body_as_json
 @handle_exceptions(handlers=[
     ExceptionHandler(Exception, GENERIC_ERROR, HTTPStatus.INTERNAL_SERVER_ERROR)
 ])
+@response_body_as_json
 @extract_from_event(parameters=[
     Parameter(path="/queryStringParameters/height", validators=[Mandatory, Type(int), Minimum(1)], transform=int),
     Parameter(path="/queryStringParameters/weight", validators=[Mandatory, Type(int), Minimum(1)], transform=int)
