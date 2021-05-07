0e1013e1657765029c96bc6e45c5c35e1488597b
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2021-05-07 12:07:50 +0100

    ready for pairing

--- src/bmi/calculator.py
+++ src/bmi/calculator.py
@@ -1,31 +1,3 @@
-from http import HTTPStatus
-import json
 
-from aws_lambda_decorators import (log, extract_from_event, Parameter, Mandatory, Minimum, Type, handle_exceptions,
-                                   ExceptionHandler, cors)
-from src.logger import get_logger
-
-
-LOGGER = get_logger(__name__)
-
-
-GENERIC_ERROR = "Internal error"
-
-
-@log(parameters=True, response=True)
-@cors(allow_origin="*", allow_methods="GET", allow_headers="Content-Type,Authorization")
-@handle_exceptions(handlers=[
-    ExceptionHandler(Exception, GENERIC_ERROR, HTTPStatus.INTERNAL_SERVER_ERROR)
-])
-@extract_from_event(parameters=[
-    Parameter(path="/queryStringParameters/height", validators=[Mandatory, Type(int), Minimum(1)], transform=int),
-    Parameter(path="/queryStringParameters/weight", validators=[Mandatory, Type(int), Minimum(1)], transform=int)
-])
-def handler(event: dict, context: dict, height: int = None, weight: int = None):  # pylint:disable=unused-argument
-    LOGGER.info("Some info message")
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "bmi": round(weight / (height / 100)**2, 2)
-        })
-    }
+def bmi_calculator():
+    pass
--- tests/bmi/test_bmi_calculator.py
+++ tests/bmi/test_bmi_calculator.py
@@ -1,19 +1,8 @@
 import unittest
-from unittest.mock import patch
-from src.bmi.calculator import handler
+from src.bmi.calculator import bmi_calculator
 
 
-class CalculatorTests(unittest.TestCase):
+class BmiCalculatorTests(unittest.TestCase):
 
-    @patch("src.bmi.calculator.LOGGER")
-    def test_success(self, mock_logger):
-        event = {
-            "queryStringParameters": {
-                "height": 180,
-                "weight": 80
-            }
-        }
-        response = handler(event, None)
-        self.assertEqual(200, response["statusCode"])
-        self.assertEqual("""{"bmi": 24.69}""", response["body"])
-        mock_logger.info.assert_called_once_with("Some info message")
+    def test_(self):
+        pass
